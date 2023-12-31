d43fd1a3d5f8192b097ac8857ecb4a5ce7a4f261
Denfit9 <79207007+Denfit9@users.noreply.github.com>
2023-12-31 23:25:58 +0300

    Added pages navigation

--- MainApp.xaml.cs
+++ MainApp.xaml.cs
@@ -28,11 +28,11 @@ namespace CinemaSchedule
         {
             InitializeComponent();
             NSP.Content = DatabaseQueries.getNSP(App.userID);
-            cinemaNameLabel.Content = "Кинотеатр  " + DatabaseQueries.getCinemaName(App.userID);
-            description.Text = DatabaseQueries.getCinemaDescription(App.userID);
+            //cinemaNameLabel.Content = "Кинотеатр  " + DatabaseQueries.getCinemaName(App.userID);
+           // description.Text = DatabaseQueries.getCinemaDescription(App.userID);
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        /*private void Button_Click(object sender, RoutedEventArgs e)
         {
             if(description.Text.Length > 0)
             {
@@ -46,13 +46,41 @@ namespace CinemaSchedule
             }
 
         }
-
+        */
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Выход совершён", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
             MainWindow login = new MainWindow();
             login.Show();
             this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ContentPanel.Children.Clear();
+            UserControls.MyCinema myCinema = new UserControls.MyCinema();
+            ContentPanel.Children.Add(myCinema);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ContentPanel.Children.Clear();
+            UserControls.Notes notes = new UserControls.Notes();
+            ContentPanel.Children.Add(notes);
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            ContentPanel.Children.Clear();
+            UserControls.Movies movies = new UserControls.Movies();
+            ContentPanel.Children.Add(movies);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            ContentPanel.Children.Clear();
+            UserControls.Schedule schedule = new UserControls.Schedule();
+            ContentPanel.Children.Add(schedule);
         }
     }
 }
---
+++ UserControls/MyCinema.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CinemaSchedule.MySQLServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CinemaSchedule.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MyCinema.xaml
+    /// </summary>
+    public partial class MyCinema : UserControl
+    {
+        public MyCinema()
+        {
+            InitializeComponent();
+            cinemaNameLabel.Content = "Кинотеатр  " + DatabaseQueries.getCinemaName(App.userID);
+        }
+    }
+}
